faccee4c30d6fcfc5ffcadace88dfc13800bac45
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-04-28 22:43:18 +0300

    Updates

---
+++ precache-manifest.b94d6912a0226476450492c8a2fc2571.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a74ed54c84013dc2513975c77bd2b20",
+    "url": "/automate-processes/index.html"
+  },
+  {
+    "revision": "e182082e94e0b6b81a1b",
+    "url": "/automate-processes/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b0aa38ac574a9ae22e86",
+    "url": "/automate-processes/static/js/2.e518caec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/automate-processes/static/js/2.e518caec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e182082e94e0b6b81a1b",
+    "url": "/automate-processes/static/js/main.8afc6a58.chunk.js"
+  },
+  {
+    "revision": "0f3ba22ad71eeca195cb",
+    "url": "/automate-processes/static/js/runtime-main.dcc36e51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/automate-processes/static/media/logo.5d5d9eef.svg"
+  }
+]);
